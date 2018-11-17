e4ad53367bc465d7886ec4e609c89f13285e8c97
Daniel Gordi <danial@gordi.ir>
2018-11-17 17:02:49 +0330

    Install Oracle JAVA JDK 8

--- install.sh
+++ install.sh
@@ -24,3 +24,20 @@ JAVA_REPOSITORY="https://ftp.weheartwebsites.de/linux/java/jdk/"
 JAVA_FILENAME="jdk-8u192-linux-x64.tar.gz"
 
 
+
+### Install Oracle Java 8
+function java_install {
+cd /opt/
+rm -rf $JAVA_FILENAME java `ls -lf1 | grep jdk`
+wget -q `echo "$JAVA_REPOSITORY""$JAVA_FILENAME"`
+tar -xf $JAVA_FILENAME && rm -f $JAVA_FILENAME
+ln -s `ls -lf1 | grep jdk` java
+update-alternatives --install /usr/bin/java java /opt/java/bin/java 1
+update-alternatives --install /usr/bin/javac javac /opt/java/bin/javac 1
+update-alternatives --install /usr/bin/javadoc javadoc /opt/java/bin/javadoc 1
+update-alternatives --install /usr/bin/jarsigner jarsigner /opt/java/bin/jarsigner 1
+update-alternatives --install /usr/bin/keytool keytool /opt/java/bin/keytool 1
+export JAVA_HOME="/opt/java/"
+echo 'JAVA_HOME="/opt/java/"' >> /etc/environment
+echo 'JAVA_HOME="/opt/java/"' >> ~/.bashrc
+}
