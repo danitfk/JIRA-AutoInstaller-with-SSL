3a3693fd5447475a53693bb03f5e93a972370387
Daniel Gordi <danial@gordi.ir>
2018-11-17 17:02:29 +0330

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Daniel Gordi (danitfk)
+# Date: 17/Nov/2018
+
+# JIRA Installation variables (must change by user)
+JIRA_USER="JIRA"
+JIRA_INSTALL_DIR="/opt/jira"
+JIRA_HOME="/var/jira/"
+JIRA_DISPLAY_NAME="Your JIRA"
+JIRA_BASE_URL="jira.gordi.ir"
+JIRA_LICENSE=""
+JIRA_SYSADMIN_USER="superuser"
+JIRA_SYSADMIN_PASSWORD="logmein2018@@"
+JIRA_SYSADMIN_DISPLAY_NAME="JIRA Superuser"
+JIRA_SYSADMIN_EMAIL_ADDRESS="superuser@mydomain.tld"
+JIRA_DATABASE_NAME="JIRA"
+JIRA_DATABASE_USERNAME="JIRAusernameDB2018"
+JIRA_DATABASE_PASSWORD="JIRApasswordDB2018"
+JIRA_PLUGIN_MIRRORING_UPSTREAM="https://bity.gordi.ir"
+JIRA_SSL_CERTIFICATE_PASS="myrandomSSLpass"
+
+# JDK 8 tar.gz Archive
+JAVA_REPOSITORY="https://ftp.weheartwebsites.de/linux/java/jdk/"
+JAVA_FILENAME="jdk-8u192-linux-x64.tar.gz"
+
+
